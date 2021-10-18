d40877ea3cb56aecb73489edeb7c195c48651550
“leonshimizu” <lmshimizu@gmail.com>
2021-10-18 16:21:49 -0700

    recipes index and create

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,17 @@
 class RecipeController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes.as_json
+  end
+
+  def create
+    recipe = Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      description: params[:description],
+      cook_time: params[:cook_time]
+    )
+    recipe.save
+    render json: recipe.as_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  recipe = Recipe.new(name: Faker::Food.dish, ingredients: Faker::Food.ingredient, description: Faker::Food.description, cook_time: Faker::Number.number(digits: 2))
+  recipe.save
+end
