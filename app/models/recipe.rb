753b8e1b12c9e64e454f68c0a6407bf15bf9c396
“leonshimizu” <lmshimizu@gmail.com>
2021-10-18 18:45:53 -0700

    friendly cook time

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,14 @@ class Recipe < ApplicationRecord
   def ingredients_list
     ingredients.split(", ")
   end
+
+  def friendly_cook_time
+    hours = cook_time / 60
+    minutes = cook_time % 60
+    if minutes == 0
+      return "It should take roughly #{hours} hours to make."
+    else 
+      return "It should take roughly #{hours} hours and #{minutes} minutes to make."
+    end
+  end
 end
